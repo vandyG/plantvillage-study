b125670b2f436ee9a7803e30b9adb028c2a6bb7f
vandyG <vandy.goel23@gmail.com>
2025-10-17 18:30:28 -0500

    feat: add data analaysis nb

---
+++ notebooks/data_analysis.py
@@ -0,0 +1,20 @@
+import marimo
+
+__generated_with = "0.17.0"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
